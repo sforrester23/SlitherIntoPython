ffdaf324b8885a153fc929c5390a086b5ceca1a6
Sam Forrester <sam_forrester@hotmail.co.uk>
2020-01-18 00:20:10 +0000

    Minor changes

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## This repo contains exercises completed in the Slither Into Python book
 
 https://www.slitherintopython.com/
+
+#### To Do: 
+- Edit chapter8/question6 to show sorting algorithm written without using the min function, it wastes computing cycles comparing min of all value when really it could find the minimum quicker by checking values one by one (and comparing to the value you've just sorted), e.g. the minimum could be found by comparing the next one, whereas using the minimum will force the comparison of all values in the list, even if the next value is the minimum
+- Edit chapter9 to reflect above concern/adjustment
+- Edit chapter12 to show similar sorting style
--- chapter8/Question6(b).py
+++ chapter8/Question6(b).py
@@ -1,13 +1,16 @@
-a = [4, 2, 9, 5, 55, 32]
+a = [909, 31, 42, 26, 15]
 
 i = 1  # Assume the first element (a[0]) is sorted
 while i < len(a):  # Same as with selection sort
     v = a[i]  # The value we want to insert into the correct position
+    print(v)
     p = i  # The position the element should be inserted into to
     while p > 0 and v < a[p - 1]:  # While value is < element to left
-        a[p] = a[p - 1]  # Move the element to left up
+        a[p] = a[p - 1]  # Move the element to left up one place
         p -= 1  # Decrement p (move one position left)
+        print(a)
     a[p] = v  # Found correct position so insert the value
+    print(a)
 
     i += 1  # Move to next element
 
